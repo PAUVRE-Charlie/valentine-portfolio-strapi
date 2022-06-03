5e9145e093a9a5d9a570e858d07e544d8fc6e436
Charlie Pauvré <chpauvre@gmail.com>
2022-06-04 00:38:28 +0200

    try fix

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: process.env.DATABASE_URL ? config.host : env("HOST", "0.0.0.0"),
+  port: process.env.DATABASE_URL ? config.port : env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
